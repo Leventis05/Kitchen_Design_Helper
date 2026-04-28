1f28c56749eb58f94c6a1c59ef428c0414a501d1
Leventis05 <nikolastarasidis@gmail.com>
2026-04-28 14:07:28 +0300

    added delete and search

--- Calendar/db_creator.py
+++ Calendar/db_creator.py
@@ -1,27 +1,51 @@
 import sqlite3
+import random as ran
+import time
+from datetime import timedelta
+import radar
+
+names = [
+    "Ralvenom", "Andmos", "Caslius", "Mavichar", "Ben", "Uka",
+    "Gad", "Lezu", "Wrerick Thundertwist", "Almin Salwuners",
+    "Lilnise", "Inlies", "Yoraborys", "Nerali", "Zenise", "Ariayola",
+    "Ariamine", "Agnelia", "Rogrea", "Yayola", "Psalm", "Fresh", "Immortal",
+    "Virtue", "Journey", "Relentless", "Despair", "Hope", "Chant"
+]
+
+pending_s = ["a", "b", "c"]
 
 conn = sqlite3.connect("test.db")
 cursor = conn.cursor()
 
+ran.seed(time.time())
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    age INTEGER,
-    date TEXT
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    name        TEXT,
+    age         INTEGER,
+    date_added  TEXT,
+    deadline    TEXT,
+    pending     TEXT
 )
 """)
 
 cursor.execute("DELETE FROM users")  # reset data (optional)
 
-cursor.executemany(
-    "INSERT INTO users (name, age, date) VALUES (?, ?, ?)",
-    [
-        ("Alice", 24, "23-05-2020"),
-        ("Bob", 30, "13-11-2025"),
-        ("Charlie", 28, "27-1-2001")
-    ]
-)
+for name in names:
+    dt = radar.random_datetime()
+    date = dt.strftime("%d-%m-%Y")
+
+    dl = dt + timedelta(days=30)
+    deadline = dl.strftime("%d-%m-%Y")
+
+    pend = ", ".join(ran.choices(pending_s, k=ran.randint(1, 3)))
+
+    cursor.execute(
+        "INSERT INTO users (name, age, date_added, deadline, pending) VALUES (?, ?, ?, ?, ?)",
+        (name, ran.randint(10, 70), date, deadline, pend)
+                )
+
 
 conn.commit()
 conn.close()
--- Calendar/gui.py
+++ Calendar/gui.py
@@ -1,20 +1,35 @@
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5.QtWidgets import QApplication, QTableView, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtWidgets import QStyledItemDelegate, QDateEdit
-from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import QStyledItemDelegate, QDateEdit,  QLineEdit, QMessageBox
+from PyQt5.QtCore import QDate, QSortFilterProxyModel, Qt
 
 def add_row():
     row = model.rowCount()
     model.insertRow(row)
-    today = QDate.currentDate().toString("dd-MM-yyyy")
-    model.setData(model.index(row, 3), today)
 
+def on_text_changed(text):
+    proxy.setFilterWildcard(text)
+
+def delete_row():
+    reply = QMessageBox.question(
+        None,
+        "Delete",
+        "Are you sure?",
+        QMessageBox.Yes | QMessageBox.No
+    )
+    p_index = view.currentIndex()
+    s_index = proxy.mapToSource(p_index)
+    if s_index.isValid():
+        model.removeRow(s_index.row())
+        model.submitAll()
+        model.select()
 
 class DateDelegate(QStyledItemDelegate):
     def createEditor(self, parent, option, index):
         editor = QDateEdit(parent)
         editor.setCalendarPopup(True)
         editor.setDisplayFormat("dd-MM-yyyy")
+        editor.setDate(QDate.currentDate())
         return editor
 
     def setEditorData(self, editor, index):
@@ -25,8 +40,6 @@ class DateDelegate(QStyledItemDelegate):
     def setModelData(self, editor, model, index):
         date = editor.date().toString("dd-MM-yyyy")
         model.setData(index, date)
-
-
 
 
 # MAIN
@@ -47,21 +60,40 @@ if __name__ == "__main__":
     model.select()            # load data
     model.setEditStrategy(QSqlTableModel.OnFieldChange)
 
+    proxy = QSortFilterProxyModel()
+    proxy.setSourceModel(model)
+    proxy.setFilterCaseSensitivity(Qt.CaseInsensitive)
+    proxy.setFilterKeyColumn(1)  # column to search (e.g. name)
+
+    date_delegate = DateDelegate()
+
     view = QTableView()
-    view.setModel(model)
+    view.setModel(proxy)
     view.horizontalHeader().setStretchLastSection(True)
-    view.setItemDelegateForColumn(3, DateDelegate())
+    view.setColumnHidden(0, True)
+    view.setItemDelegateForColumn(3, date_delegate)
+    view.setItemDelegateForColumn(4, date_delegate)
+
+    search = QLineEdit()
+    search.setPlaceholderText("Search...")
+
+    search.textChanged.connect(on_text_changed)
 
     add_tuple_btn = QPushButton("Add Row")
 
-    add_tuple_btn.clicked.connect(add_row)
+    dltRowButton = QPushButton("Delete Row")
 
+    add_tuple_btn.clicked.connect(add_row)
+    dltRowButton.clicked.connect(delete_row)
+
+    layout.addWidget(search)
     layout.addWidget(view)
     layout.addWidget(add_tuple_btn)
+    layout.addWidget(dltRowButton)
 
 
     window.setLayout(layout)
-    window.resize(800, 500)
+    window.resize(1000, 700)
     window.show()
     app.exec_()
 
